e30895a0636db5993ec781285892aeb388260f6b
hwandong-joo <hwandong-joo@godo.co.kr>
2021-01-19 18:23:26 +0900

    add #8 - entity to dto 2

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -3,13 +3,19 @@ package org.zerock.guestbook.service;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 @Service
 @Log4j2
-public class GuestbookSeriveImpl implements GuestbookService {
+public class GuestbookServiceImpl implements GuestbookService {
 
     @Override
     public Long register(GuestbookDTO dto) {
+        log.info("DTO---------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
         return null;
     }
 }
